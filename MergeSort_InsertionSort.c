840cb2be686c9544bb36d7efd495000febcdbf68
taepop <113758389+taepop@users.noreply.github.com>
2023-02-18 15:20:52 +0800

    Add files via upload

---
+++ MergeSort_InsertionSort.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void insertion_sort(int arr[], int n, int s) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int arr[], int l, int r, int s) {
+    if (r - l + 1 <= s) {
+        insertion_sort(arr + l, r - l + 1, s);
+    }
+    else {
+        int m = l + (r - l) / 2;
+        merge_sort(arr, l, m, s);
+        merge_sort(arr, m + 1, r, s);
+        merge(arr, l, m, r);
+    }
+}
